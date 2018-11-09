8353c71e836a4885824e1ba5c4c38bbdd9ee3831
Claire Gilligan <feminaprovita@gmail.com>
2018-11-09 14:32:57 -0500

    sequelize schema

--- models/index.js
+++ models/index.js
@@ -1,6 +1,60 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isAlphanumeric: true,
+      notContains: ' ',
+    },
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  status: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      defaultValue: true,
+      // ???????
+    },
+  },
+  // if we can use true / false to mean open / closed ?????
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+});
+
 module.exports = {
-  db
+  db,
 };
