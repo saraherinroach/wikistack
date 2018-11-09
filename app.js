a4fad2ff4fc21dfedf99af7c157d06236d1fafa3
Sarah Erin Roach <saraherinroach@gmail.com>
2018-11-09 12:34:21 -0500

    more setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 // const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { db } = require('./models');
+
+const layout = require('./views/layout.js')
 
 app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
@@ -10,10 +13,20 @@ app.use(morgan('dev'));
 /**/ app.use(express.static(__dirname + '/client'));
 
 app.get('/', (req, res, next) => {
-  res.send('hello world');
+  // res.send('hello world');
+  res.send(layout(''))
+});
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
 });
 
 const PORT = 2211;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+
+
+
